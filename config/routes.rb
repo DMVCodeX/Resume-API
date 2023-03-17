0f08259b5995095cebf228d90b419656a23df840
DIANELLY VENTURA <DMVCode@outlook.com>
2023-03-16 20:09:32 -0400

    Add jbuilder for students

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "student#index"
 
   get "/students" => "students#index"
   get "/students" => "students#show"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Student.create(first_name: "Jose", last_name: "Rodriguez", email: "jose@test.com", phone_number: "123-456-789", short_bio: "Full Stack Developer", linkedin_url: "https://linkedIn.com/joserodriguez", twitter_handle: "jose_rodriguez", website_url: "joserodriguez.com", online_resume: "jose-resume", github_url: "https://github.com/JoseMRodriguezO", photo: "https://avatars.githubusercontent.com/u/116036287?v=4")
